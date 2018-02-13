957080d3adafa27489e52c3b692524eb7201e712
Claudio Acioli <kllaudyo@gmail.com>
2018-02-13 21:52:18 -0200

    feat: Adiciona api

---
+++ src/utils/api.js
@@ -0,0 +1,11 @@
+const base_url = "http://localhost:3001";
+const headers = {
+    'Content-Type':'application/json',
+    'Accept':'application/json',
+    'Authorization': Math.random().toString(36).substr(-8)
+};
+
+export const
+    getCategories = () =>
+        fetch(`${base_url}/categories`, { headers })
+            .then(res => res.json());
