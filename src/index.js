5018b2b83433c81c7695d2e4174991a6bbd76348
Claudio Acioli <kllaudyo@gmail.com>
2018-02-13 22:45:04 -0200

    feat: Cria store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import { categories } from './reducers';
+import { fetchCategories } from './actions';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(
+    combineReducers({categories}),
+    applyMiddleware(thunk)
+);
+
+store.subscribe( () => console.log(store.getState()));
+store.dispatch(fetchCategories());
+console.log(store.getState());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
