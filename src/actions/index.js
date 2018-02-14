69891cbd23c51069c39842533a2301f359629542
Claudio Acioli <kllaudyo@gmail.com>
2018-02-13 22:44:47 -0200

    feat: Adiciona action creator assincrono

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,17 @@
 import C from '../utils/constants';
+import * as API from '../utils/api';
 
 export const addCategory = (name, path) => ({
     type: C.ADD_CATEGORY,
     name,
     path
 });
+
+export const
+    fetchCategories = () => dispatch =>
+        API.getCategories()
+            .then( ({categories}) =>
+                categories.map(
+                    ({name, path}) => dispatch(addCategory(name, path))
+                )
+            );
