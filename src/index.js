72b5df3cd10253b3b31d694f46dac61fa9de455a
Claudio Acioli <kllaudyo@gmail.com>
2018-02-17 20:56:29 -0200

    refactor: Separa a store da index

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
-import reducer from './reducers';
-import { fetchCategories, fetchPosts } from './actions';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(
-    reducer,
-    composeWithDevTools(
-        applyMiddleware(thunk)
-    )
-);
-
-store.subscribe( () => console.log(store.getState()));
-store.dispatch(fetchCategories());
-store.dispatch(fetchPosts());
 console.log(store.getState());
 
 ReactDOM.render(<App />, document.getElementById('root'));
