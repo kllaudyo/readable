40ec7bd6197b1ffd3c118d25de293a99dff7b711
Claudio Acioli <kllaudyo@gmail.com>
2018-02-18 04:16:16 -0300

    feat: Implementa Providers para redux e thema

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { MuiThemeProvider } from 'material-ui/styles';
 import store from './store';
+import theme from './theme';
 import ReadableApp from './ReadableApp';
 import registerServiceWorker from './registerServiceWorker';
 
-console.log(store.getState());
-
 ReactDOM.render(
-    <ReadableApp />,
+    <MuiThemeProvider theme={theme}>
+        <Provider store={store}>
+            <ReadableApp />
+        </Provider>
+    </MuiThemeProvider>,
     document.getElementById('root')
 );
 
