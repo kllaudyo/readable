5dd05a9e26a112a93cfc00561a9f21683f88878f
Claudio Acioli <kllaudyo@gmail.com>
2018-02-18 04:13:13 -0300

    refactor: Altera a localização do thema

---
+++ src/theme/index.js
@@ -0,0 +1,16 @@
+import blue from "material-ui/colors/blue";
+import deepPurple from "material-ui/colors/deepPurple";
+import {createMuiTheme} from "material-ui/styles/index";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: blue,
+        secondary: deepPurple,
+    },
+    status: {
+        deepPurple,
+        danger: 'orange',
+    }
+});
+
+export default theme;
