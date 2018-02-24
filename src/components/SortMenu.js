8ce2f25595ef9ce06fb76414964450348af0e244
Claudio Acioli <kllaudyo@gmail.com>
2018-02-23 22:22:09 -0300

    feat: Adiciona componente SortMenu

---
+++ src/components/SortMenu.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+    Dialog,
+    DialogTitle,
+    List,
+    ListItem,
+    ListItemText,
+    ListItemIcon,
+} from 'material-ui';
+import CheckedIcon from 'react-icons/lib/md/radio-button-checked';
+import UnCheckedIcon from 'react-icons/lib/md/radio-button-unchecked';
+
+const sortOptions = [
+    {value:'date',text:'Date'},
+    {value:'author',text:'Author'},
+];
+
+const SortMenu = ({open, sortBy="date", onClose, onSortBy}) => (
+    <Dialog
+        aria-labelledby="simple-dialog-title"
+        open={open}
+        onClose={onClose}>
+        <DialogTitle id="simple-dialog-title">Sort By</DialogTitle>
+        <div>
+            <List>
+                {sortOptions.map( ({value, text}) =>
+                    <ListItem key={value} button onClick={() => onSortBy(value)}>
+                        <ListItemIcon>
+                            {sortBy === value && (
+                                <CheckedIcon />
+                            ) || (
+                                <UnCheckedIcon />
+                            )}
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                )}
+            </List>
+        </div>
+    </Dialog>
+);
+
+export default SortMenu;
