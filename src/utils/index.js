40e5e0c656b5bd6a3576cbecebbba97deba5aa3b
Claudio Acioli <kllaudyo@gmail.com>
2018-03-01 07:08:13 -0300

    feat: Cria funcoes uteis

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,29 @@
 import { compose } from 'redux'
 
-export const getFirstArrayItem = array => array[0];
+export const
 
-export const filterArrayById = (array, id) =>
-    array.filter(item => item.id === id);
+    getFirstArrayItem = array => array[0],
 
-export const findById = compose(
-    getFirstArrayItem,
-    filterArrayById
-);
+    filterArrayById = (array, id) =>
+        array.filter(item => item.id === id),
+
+    filterArrayByPath = (array, path) =>
+        array.filter(item => item.path === path),
+
+    filterArrayByParentId = (array, parentId) =>
+        array.filter(item => item.parentId === parentId),
+
+    findById = compose(
+        getFirstArrayItem,
+        filterArrayById
+    ),
+
+    findByPath = compose(
+        getFirstArrayItem,
+        filterArrayByPath
+    ),
+
+    findByParentId = compose(
+        getFirstArrayItem,
+        filterArrayByParentId
+    );
