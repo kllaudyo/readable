e86018620dc7f06e3772f3760fceb88576b13752
Claudio Acioli <kllaudyo@gmail.com>
2018-03-02 16:50:07 -0300

    refactor: Usa arquivo principal de estilos

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { withStyles } from 'material-ui/styles/index';
 import { ListItem, ListItemText } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
 import Avatar from 'material-ui/Avatar';
 import Person from 'react-icons/lib/md/person';
-import lightBlue from 'material-ui/colors/lightBlue';
+import classes from '../classes';
 
-const PostItem = ({id, author, title, date}) => (
+const PostItem = ({classes, id, author, title, date}) => (
     <React.Fragment>
-        <ListItem button component={Link} to={`post/${id}`}>
-            <Avatar style={{backgroundColor:lightBlue[200]}}>
+        <ListItem button component={ Link } to={`/post/${id}`}>
+            <Avatar className={classes.avatar}>
                 <Person />
             </Avatar>
             <ListItemText
@@ -21,4 +22,4 @@ const PostItem = ({id, author, title, date}) => (
     </React.Fragment>
 );
 
-export default PostItem;
+export default withStyles(classes)(PostItem);
