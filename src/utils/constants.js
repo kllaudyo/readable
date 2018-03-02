c174c7ddfa652087a92551c1b2b93594b16dff3b
Claudio Acioli <kllaudyo@gmail.com>
2018-03-02 15:57:09 -0300

    feat: Adiciona nova constante

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,10 @@
 const constants = {
     ADD_CATEGORY: 'ADD_CATEGORY',
     ADD_POST: 'ADD_POST',
-    ADD_COMMENT: 'ADD_COMMENT'
+    ADD_COMMENT: 'ADD_COMMENT',
+    SORT_POSTS: 'SORT_POSTS',
+    SORTED_BY_VOTE_SCORE: '-voteScore',
+    SORTED_BY_DATE: '-timestamp'
 };
 
 export default constants;
