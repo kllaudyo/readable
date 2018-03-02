cbaf6c294cf48b7df7ce01a9f1a4bbf079402598
Claudio Acioli <kllaudyo@gmail.com>
2018-03-02 19:12:27 -0300

    feat: Adiciona controle e exibição para voteScore

--- src/components/CommentItem.js
+++ src/components/CommentItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { withStyles } from 'material-ui/styles/index';
-import { ListItem, ListItemText } from 'material-ui/List';
-import Avatar from 'material-ui/Avatar';
+import { Avatar, ListItem, ListItemText, ListItemSecondaryAction, IconButton, Badge } from 'material-ui';
 import Person from 'react-icons/lib/md/person';
+import ThumbDownIcon from "react-icons/lib/md/thumb-down";
+import ThumbUpIcon from "react-icons/lib/md/thumb-up";
+import HeartIcon from 'react-icons/lib/md/favorite';
 import classes from '../classes';
 
-
-const CommentItem = ({id, author, body, date, classes}) => (
+const CommentItem = ({id, author, body, date, voteScore, classes, onPositiveComment, onNegativeComment}) => (
     <React.Fragment>
         <ListItem button>
             <Avatar className={classes.avatar}>
@@ -16,6 +17,19 @@ const CommentItem = ({id, author, body, date, classes}) => (
                 primary={body}
                 secondary={`${author} - ${date}`}
             />
+            <ListItemSecondaryAction>
+                <IconButton>
+                    <Badge badgeContent={voteScore} color="secondary">
+                        <HeartIcon/>
+                    </Badge>
+                </IconButton>
+                <IconButton onClick={()=>onNegativeComment({id})}>
+                    <ThumbDownIcon />
+                </IconButton>
+                <IconButton onClick={()=>onPositiveComment({id})}>
+                    <ThumbUpIcon />
+                </IconButton>
+            </ListItemSecondaryAction>
         </ListItem>
     </React.Fragment>
 );
