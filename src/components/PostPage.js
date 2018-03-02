e23749538af10dd63282396642a59e4ebab896ee
Claudio Acioli <kllaudyo@gmail.com>
2018-03-02 19:12:57 -0300

    fix: Corrige ordenação de comentarios

--- src/components/PostPage.js
+++ src/components/PostPage.js
@@ -1,13 +1,16 @@
 import { connect } from 'react-redux';
 import { withStyles } from 'material-ui/styles';
 import { filterArrayByParentId, findById } from "../utils";
+import sortBy from 'sort-by';
 import PostDetails from './PostDetails';
 import classes from '../classes';
+import C from "../utils/constants";
 
 export default connect(
     ({posts, comments}, ownProps) => ({
         post: findById(posts, ownProps.id),
-        comments: filterArrayByParentId(comments, ownProps.id),
+        comments: filterArrayByParentId(comments, ownProps.id)
+            .sort(sortBy(C.SORTED_BY_VOTE_SCORE)),
         ...ownProps
     })
 )(withStyles(classes)(PostDetails));
