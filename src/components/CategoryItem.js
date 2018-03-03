77daebb272b307f8f122ed03d5f520a28e8664c2
Claudio Acioli <kllaudyo@gmail.com>
2018-03-03 06:24:40 -0300

    feat: Controla propriedades do componente

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
 import { ListItem, ListItemIcon, ListItemText } from 'material-ui';
 import CategoryIcon from 'react-icons/lib/md/local-offer';
@@ -11,5 +12,10 @@ const CategoryItem = ({name, path}) => (
         <ListItemText primary={name} />
     </ListItem>
 );
+
+CategoryIcon.propTypes = {
+    name: PropTypes.string.isRequired,
+    path: PropTypes.string.isRequired
+};
 
 export default CategoryItem;
