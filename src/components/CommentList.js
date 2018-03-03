52b08920ac2a75a97c331d48238ac8d869f9e692
Claudio Acioli <kllaudyo@gmail.com>
2018-03-03 07:04:55 -0300

    feat: Adiciona controle propriedades componente CommentList

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {List, ListSubheader} from 'material-ui';
 import CommentItem from "./CommentItem";
 
@@ -16,5 +17,27 @@ const CommentList = ({comments, onOpenForm, onPositiveComment, onNegativeComment
         )}
     </List>
 );
+
+CommentList.propTypes = {
+    comments: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        author: PropTypes.string.isRequired,
+        body: PropTypes.string.isRequired,
+        timestamp: PropTypes.number.isRequired,
+        voteScore: PropTypes.number.isRequired
+    })).isRequired,
+    onOpenForm: PropTypes.func.isRequired,
+    onPositiveComment: PropTypes.func.isRequired,
+    onNegativeComment: PropTypes.func.isRequired,
+    onDeleteComment: PropTypes.func.isRequired
+};
+
+CommentList.defaultProps = {
+    comments: [],
+    onOpenForm: f=>f,
+    onPositiveComment: f=>f,
+    onNegativeComment: f=>f,
+    onDeleteComment: f=>f
+};
 
 export default CommentList;
