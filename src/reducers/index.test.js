ab0eec3ddb19ed39c88d092ddea876a42a69ba6a
Claudio Acioli <kllaudyo@gmail.com>
2018-03-05 21:52:01 -0300

    test: Garante imutabilidade do state

--- src/reducers/index.test.js
+++ src/reducers/index.test.js
@@ -1,4 +1,5 @@
 import C from '../utils/constants';
+import deepFreeze from 'deep-freeze';
 import { category } from './index';
 
 describe("Category reducer", ()=> {
@@ -9,6 +10,8 @@ describe("Category reducer", ()=> {
            name:'udacity',
            path:'udacity'
        };
+       deepFreeze(state);
+       deepFreeze(action);
        const results = category(state, action);
        expect(results)
            .toEqual({
