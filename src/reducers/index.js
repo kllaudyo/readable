d9d10723f21f45e78c709f170ce959eb76156b55
Claudio Acioli <kllaudyo@gmail.com>
2018-03-05 21:40:46 -0300

    refactor: Altera combinação de reducers entre arquivos

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,131 +1,119 @@
-import { combineReducers } from 'redux';
 import C from '../utils/constants';
 
-const category = (state={}, action) => {
-    const { name, path } = action;
-    switch(action.type){
-        case C.ADD_CATEGORY:
-            return {
-                name,
-                path
-            };
-        default:
-            return state;
-    }
-};
+export const
 
-const categories = (state=[], action) => {
-    switch(action.type){
-        case C.ADD_CATEGORY:
-            return [
-                ...state,
-                category({}, action)
-            ];
-        default:
-            return state;
-    }
-};
+    category = (state={}, action) => {
+        const { name, path } = action;
+        switch(action.type){
+            case C.ADD_CATEGORY:
+                return {
+                    name,
+                    path
+                };
+            default:
+                return state;
+        }
+    },
 
-const post = (state={}, action) => {
-    const { id, timestamp, title, body, author, category, voteScore, deleted, commentCount } = action;
-    switch(action.type){
-        case C.ADD_POST:
-            return {
-                id,
-                timestamp,
-                title,
-                body,
-                author,
-                category,
-                voteScore,
-                deleted,
-                commentCount
-            };
-        case C.EDIT_POST:
-            return (state.id !== id) ?
-                state :
-                {
+    categories = (state=[], action) => {
+        switch(action.type){
+            case C.ADD_CATEGORY:
+                return [
                     ...state,
+                    category({}, action)
+                ];
+            default:
+                return state;
+        }
+    },
+
+    post = (state={}, action) => {
+        const { id, timestamp, title, body, author, category, voteScore, deleted, commentCount } = action;
+        switch(action.type){
+            case C.ADD_POST:
+                return {
+                    id,
                     timestamp,
                     title,
                     body,
                     author,
                     category,
+                    voteScore,
+                    deleted,
+                    commentCount
                 };
-        case C.POST_VOTE_SCORE:
-            return (state.id !== id) ?
-                state :
-                {
-                    ...state,
-                    voteScore
-                };
-        case C.ADD_COMMENT:
-            return state.id !== id ?
-                state :
-                {
-                    ...state,
-                    id,
-                    commentCount: state.commentCount+1
-                };
-        case C.DELETE_POST:
-            return state.id !== id ?
-                state :
-                {
-                    ...state,
-                    deleted
-                };
-        case C.DELETE_COMMENT:
-            return state.id !== id ?
-                state :
-                {
-                    ...state,
-                    id,
-                    commentCount: state.commentCount-1
-                };
-        default:
-            return state;
-    }
-};
+            case C.EDIT_POST:
+                return (state.id !== id) ?
+                    state :
+                    {
+                        ...state,
+                        timestamp,
+                        title,
+                        body,
+                        author,
+                        category,
+                    };
+            case C.POST_VOTE_SCORE:
+                return (state.id !== id) ?
+                    state :
+                    {
+                        ...state,
+                        voteScore
+                    };
+            case C.ADD_COMMENT:
+                return state.id !== id ?
+                    state :
+                    {
+                        ...state,
+                        id,
+                        commentCount: state.commentCount+1
+                    };
+            case C.DELETE_POST:
+                return state.id !== id ?
+                    state :
+                    {
+                        ...state,
+                        deleted
+                    };
+            case C.DELETE_COMMENT:
+                return state.id !== id ?
+                    state :
+                    {
+                        ...state,
+                        id,
+                        commentCount: state.commentCount-1
+                    };
+            default:
+                return state;
+        }
+    },
 
-const posts = (state=[], action) => {
-    switch(action.type){
-        case C.ADD_POST:
-            return [
-                ...state,
-                post({}, action)
-            ];
-        case C.EDIT_POST:
-        case C.DELETE_POST:
-        case C.POST_VOTE_SCORE:
-        case C.ADD_COMMENT:
-        case C.DELETE_COMMENT:
-            return state.map(
-                p => post(p, action)
-            );
-        default:
-            return state;
-    }
-};
-
-const comment = (state={}, action) => {
-    const {id, parentId, timestamp, body, author, voteScore, deleted, parentDeleted} = action;
-    switch(action.type){
-        case C.ADD_COMMENT:
-            return {
-                id,
-                parentId,
-                timestamp,
-                body,
-                author,
-                voteScore,
-                deleted,
-                parentDeleted
-            };
-        case C.EDIT_COMMENT:
-            return state.id !== id ?
-                state :
-                {
+    posts = (state=[], action) => {
+        switch(action.type){
+            case C.ADD_POST:
+                return [
                     ...state,
+                    post({}, action)
+                ];
+            case C.EDIT_POST:
+            case C.DELETE_POST:
+            case C.POST_VOTE_SCORE:
+            case C.ADD_COMMENT:
+            case C.DELETE_COMMENT:
+                return state.map(
+                    p => post(p, action)
+                );
+            default:
+                return state;
+        }
+    },
+
+    comment = (state={}, action) => {
+        const {id, parentId, timestamp, body, author, voteScore, deleted, parentDeleted} = action;
+        switch(action.type){
+            case C.ADD_COMMENT:
+                return {
+                    id,
                     parentId,
                     timestamp,
                     body,
@@ -134,51 +122,62 @@ const comment = (state={}, action) => {
                     deleted,
                     parentDeleted
                 };
-        case C.DELETE_COMMENT:
-            return state.id !== id ?
-                state :
-                {
+            case C.EDIT_COMMENT:
+                return state.id !== id ?
+                    state :
+                    {
+                        ...state,
+                        parentId,
+                        timestamp,
+                        body,
+                        author,
+                        voteScore,
+                        deleted,
+                        parentDeleted
+                    };
+            case C.DELETE_COMMENT:
+                return state.id !== id ?
+                    state :
+                    {
+                        ...state,
+                        deleted,
+                    };
+            case C.COMMENT_VOTE_SCORE:
+                return state.id !== id ?
+                    state :
+                    {
+                        ...state,
+                        voteScore
+                    };
+            default:
+                return state;
+        }
+    },
+
+    comments = (state=[], action) => {
+        switch(action.type){
+            case C.ADD_COMMENT:
+                return [
                     ...state,
-                    deleted,
-                };
-        case C.COMMENT_VOTE_SCORE:
-            return state.id !== id ?
-                state :
-                {
-                    ...state,
-                    voteScore
-                };
-        default:
-            return state;
-    }
-};
+                    comment({}, action)
+                ];
+            case C.EDIT_COMMENT:
+            case C.DELETE_COMMENT:
+            case C.COMMENT_VOTE_SCORE:
+                return state.map(
+                    c => comment(c, action)
+                );
+            default:
+                return state;
+        }
+    },
 
-const comments = (state=[], action) => {
-    switch(action.type){
-        case C.ADD_COMMENT:
-            return [
-                ...state,
-                comment({}, action)
-            ];
-        case C.EDIT_COMMENT:
-        case C.DELETE_COMMENT:
-        case C.COMMENT_VOTE_SCORE:
-            return state.map(
-                c => comment(c, action)
-            );
-        default:
-            return state;
-    }
-};
-
-const sort = (state=C.SORTED_BY_VOTE_SCORE, action) => {
-    const { sortBy } = action;
-    switch(action.type){
-        case C.SORT_POSTS:
-            return sortBy;
-        default:
-            return state;
-    }
-};
-
-export default combineReducers({categories, posts, comments, sort});
+    sort = (state=C.SORTED_BY_VOTE_SCORE, action) => {
+        const { sortBy } = action;
+        switch(action.type){
+            case C.SORT_POSTS:
+                return sortBy;
+            default:
+                return state;
+        }
+    };
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware} from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import reducer from '../reducers';
+import {categories, posts, comments, sort} from '../reducers';
 
 export default createStore(
-    reducer,
+    combineReducers({categories, posts, comments, sort}),
     composeWithDevTools(
         applyMiddleware(thunk)
     )
